8c03414c6850aaf52dddcbc2516fa8d073ba8879
iwai32 <iwakou32@gmail.com>
2022-01-07 03:37:45 +0900

    PlayerのViewを作成

---
+++ Assets/MyGames/Scripts/View/PlayerView/PointView.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointView : MonoBehaviour
+{
+    [SerializeField]
+    [Header("ポイントのUIプレハブを設定します")]
+    GameObject pointPrefab;
+
+    [SerializeField]
+    [Header("ポイントUIの格納場所を設定")]
+    Transform pointTransform;
+
+    /// <summary>
+    /// ポイントのセット
+    /// </summary>
+    public void SetPointGauge(int point)
+    {
+        //ポイントを一旦削除
+        for (int i = 0; i < pointTransform.childCount; i++)
+        {
+            Destroy(pointTransform.GetChild(i).gameObject);
+        }
+
+        //追加していく
+        for (int i = 0; i < point; i++)
+        {
+            Instantiate(pointPrefab, pointTransform);
+        }
+    }
+}
---
+++ Assets/MyGames/Scripts/View/PlayerView/ScoreView.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreView : MonoBehaviour
+{
+    [SerializeField]
+    [Header("スコア表示用テキストを設定")]
+    Text scoreText;
+    
+    /// <summary>
+    /// Scoreの更新
+    /// </summary>
+    public void SetScore(int score)
+    {
+        scoreText.text = score.ToString();
+    }
+}
