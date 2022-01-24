a4af41dab0e203f404e69668f067e270d7dd8ae4
iwai32 <iwakou32@gmail.com>
2022-01-25 03:43:37 +0900

    タイトルシーンのスクリプトを作り直す

---
+++ Assets/MyGames/Scripts/Title/Scripts/Title.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+using Cysharp.Threading.Tasks;
+using Zenject;
+using CustomSceneManager;
+using UIUtility;
+
+namespace Title
+{
+    public class Title : MonoBehaviour
+    {
+        [SerializeField]
+        [Header("スタートボタンのテキスト")]
+        Text _startButtonText;
+
+        [SerializeField]
+        [Header("StartButtonを設定")]
+        Button _startButton;
+
+        [SerializeField]
+        [Header("ボタンの点滅速度")]
+        float _blinkSpeed = 1.0f;
+
+        [SerializeField]
+        [Header("ボタンの点滅回数")]
+        float _blinkCount = 5.0f;
+
+        [SerializeField]
+        [Header("ボタンの点滅時間")]
+        float _maxBlinkTime = 1.0f;
+
+        [SerializeField]
+        [Header("クリック時のSE")]
+        AudioClip _clickSE;
+
+
+        float _elapsedBlinkTime;
+        float _blinkTime;
+
+
+        #region//フィールド
+        ICustomSceneManager _customSceneManager;
+        IObservableClickButton _observableClickButton;
+        #endregion
+
+        [Inject]
+        public void Construct(
+            ICustomSceneManager customSceneManager,
+            IObservableClickButton observableClickButton
+        )
+        {
+            _customSceneManager = customSceneManager;
+            _observableClickButton = observableClickButton;
+        }
+
+        void Start()
+        {
+            Bind();
+        }
+
+        void Bind()
+        {
+            _observableClickButton
+                .CreateObservableClickButton(_startButton)
+                .Subscribe(_ =>
+                {
+                    StartGame().Forget();
+                })
+                .AddTo(this);
+        }
+
+        async UniTask StartGame()
+        {
+            await BlinkClickedButton();
+
+            _customSceneManager.LoadScene(SceneType.STAGE);
+        }
+
+        /// <summary>
+        /// ボタンの点滅
+        /// </summary>
+        /// <returns></returns>
+        async UniTask BlinkClickedButton()
+        {
+            while (_blinkTime <= _maxBlinkTime)
+            {
+                _startButtonText.color = GetAlphaColor(_startButtonText.color);
+                _blinkTime += Time.deltaTime;
+                await UniTask.Delay(TimeSpan.FromSeconds(Time.deltaTime));
+            }
+        }
+
+        /// <summary>
+        /// アルファ値を取得
+        /// </summary>
+        /// <param name="color"></param>
+        /// <returns></returns>
+        Color GetAlphaColor(Color color)
+        {
+            _elapsedBlinkTime += Time.deltaTime * _blinkCount * _blinkSpeed;
+            color.a = Mathf.Sin(_elapsedBlinkTime);
+            return color;
+        }
+    }
+
+}
