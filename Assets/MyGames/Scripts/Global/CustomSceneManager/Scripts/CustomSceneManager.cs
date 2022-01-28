9edc2027f117f362c4c7e95f4d6c1763da555b32
iwai32 <iwakou32@gmail.com>
2022-01-29 04:53:01 +0900

    フェードインとフェードアウト処理を、タイトルシーンとステージシーンに紐付ける

--- Assets/MyGames/Scripts/Global/CustomSceneManager/Scripts/CustomSceneManager.cs
+++ Assets/MyGames/Scripts/Global/CustomSceneManager/Scripts/CustomSceneManager.cs
@@ -1,16 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Fade;
+using Cysharp.Threading.Tasks;
+using Zenject;
 
 namespace CustomSceneManager
 {
     public class CustomSceneManager : MonoBehaviour, ICustomSceneManager
     {
+        IFade _fade;
+
+        [Inject]
+        public void Construct(IFade fade)
+        {
+            _fade = fade;
+        }
+
         public void LoadScene(SceneType sceneName)
         {
-            //todo フェードを出現させる
-            SceneManager.LoadScene(CommonAttribute.GetStringValue(sceneName));
+            //引数のあるAction型を呼び出し先に渡すための整形処理
+            Action action = () =>
+            {
+                SceneManager.LoadScene(CommonAttribute.GetStringValue(sceneName));
+            };
+
+            //フェードアウト後シーンを読み込みます
+            _fade.FadeOutBeforeAction(action).Forget();
         }
     }
 }
