8c4553f80bb249778319363df825ed1f4a3bedc4
iwai32 <iwakou32@gmail.com>
2022-02-04 07:06:49 +0900

    UIで使用する共通処理(クリックボタンや表示のOn, Off)はプロジェクト間で使用するため、ProjectPrefabInstallerに移行する

--- Assets/MyGames/Scripts/Installer/GlobalInstaller/ProjectPrefabInstaller.cs
+++ Assets/MyGames/Scripts/Installer/GlobalInstaller/ProjectPrefabInstaller.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
 using Zenject;
 using Fade;
+using Dialog;
 using CustomSceneManager;
 using SaveDataManager;
 using SoundManager;
+using UIUtility;
 
 namespace GlobalInstaller
 {
@@ -25,6 +27,10 @@ namespace GlobalInstaller
         [Header("サウンドマネージャーのプレハブを設定")]
         SoundManager.SoundManager _soundManagerPrefab;
 
+        [SerializeField]
+        [Header("ダイアログのプレハブを設定")]
+        Dialog.Dialog _dialogPrefab;
+
         /// <summary>
 		/// シーンの切り替えを行っても破棄しないインスタンスを生成します
 		/// </summary>
@@ -34,11 +40,6 @@ namespace GlobalInstaller
                .FromComponentInNewPrefab(_customSceneManagerPrefab)
                .AsSingle()
                .NonLazy();
-
-            Container.Bind<IFade>()
-                .FromComponentInNewPrefab(_fadePrefab)
-                .AsSingle()
-                .NonLazy();
 
             Container.Bind<ISaveDataManager>()
                 .FromComponentInNewPrefab(_saveDataManagerPrefab)
@@ -50,8 +51,33 @@ namespace GlobalInstaller
                 .AsSingle()
                 .NonLazy();
 
+            //ダイアログ
+            Container.Bind<IDialog>()
+                .FromComponentInNewPrefab(_dialogPrefab)
+                .AsSingle()
+                .NonLazy();
+
+            //フェード
+            Container.Bind<IFade>()
+                .FromComponentInNewPrefab(_fadePrefab)
+                .AsSingle()
+                .NonLazy();
+
+            //セーブデータ
             Container.Bind<ISaveData>()
                 .To<SaveData>()
+                .AsSingle()
+                .NonLazy();
+
+            //クリックボタン
+            Container.Bind<IObservableClickButton>()
+                .To<ObservableClickButton>()
+                .AsSingle()
+                .NonLazy();
+
+            //UIの表示非表示処理
+            Container.Bind<IToggleableUI>()
+                .To<ToggleableUI>()
                 .AsSingle()
                 .NonLazy();
         }
--- Assets/MyGames/Scripts/Installer/GlobalInstaller/SceneMonoInstaller.cs
+++ Assets/MyGames/Scripts/Installer/GlobalInstaller/SceneMonoInstaller.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 using Zenject;
-using UIUtility;
 using CountDownTimer;
 
 namespace GlobalInstaller
@@ -9,12 +8,6 @@ namespace GlobalInstaller
     {
         public override void InstallBindings()
         {
-            //クリックボタン
-            Container.Bind<IObservableClickButton>()
-                .To<ObservableClickButton>().AsSingle().NonLazy();
-            //UIの表示非表示処理
-            Container.Bind<IToggleableUI>()
-                .To<ToggleableUI>().AsSingle().NonLazy();
             //カウントダウン
             Container.Bind<IObservableCountDownTimer>()
                 .To<ObservableCountDownTimer>().AsTransient().NonLazy();
