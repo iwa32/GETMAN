6b65ae1a37dbc145f6b2c800df4602b30bbc8601
iwai32 <iwakou32@gmail.com>
2022-04-13 20:03:14 +0900

    SP武器未取得時はUIを透明にします

--- Assets/MyGames/Projects/Player/View/Scripts/SpWeaponView.cs
+++ Assets/MyGames/Projects/Player/View/Scripts/SpWeaponView.cs
@@ -11,13 +11,32 @@ namespace PlayerView
         [Header("SP武器のアイコン表示用UIを設定")]
         Image _spWeaponImage;
 
+        int OPACITY_ZERO = 0;
+        int OPACITY_MAX = 255;
+
+        void Awake()
+        {
+            SetIconColorAlpha(OPACITY_ZERO);//最初はアイコンを透明にします。
+        }
+
         /// <summary>
         /// sp武器のアイコンを設定します
         /// </summary>
         /// <param name="icon"></param>
         public void SetIcon(Sprite icon)
         {
+            //透明なら見えるようにする
+            if (_spWeaponImage.color.a != OPACITY_MAX)
+                SetIconColorAlpha(OPACITY_MAX);
+            
             _spWeaponImage.sprite = icon;
+        }
+
+        void SetIconColorAlpha(int alphaValue)
+        {
+            Color color = _spWeaponImage.color;
+            color.a = alphaValue;
+            _spWeaponImage.color = color;
         }
     }
 }
