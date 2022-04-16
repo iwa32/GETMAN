20f867950065a16e6e06f567c614ab8a1f0e858a
iwai32 <iwakou32@gmail.com>
2022-04-17 05:31:02 +0900

    ミュート時アイコン変更機能を追加、イベントのObervable化処理をviewに移行

--- Assets/MyGames/Projects/Global/Sound/Presenter/SoundPresenter.cs
+++ Assets/MyGames/Projects/Global/Sound/Presenter/SoundPresenter.cs
@@ -1,10 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UniRx;
 using Zenject;
-using UIUtility;
 using SoundView;
 using SoundModel;
 
@@ -13,83 +11,49 @@ namespace SoundPresenter
     public class SoundPresenter : MonoBehaviour
     {
         [SerializeField]
-        [Header("開くボタン(サウンドオプション)")]
-        Button _buttonToOpenCanvas;
-
-        [SerializeField]
-        [Header("閉じるボタン(サウンドオプション)")]
-        Button _buttonToCloseCanvas;
-
-        [SerializeField]
-        [Header("閉じるためのオーバーレイを設定します(サウンドオプション)")]
-        Button _overlayToCloseCanvas;
-
-        [SerializeField]
-        [Header("Bgmミュート用ボタンを設定")]
-        Button _bgmMuteButton;
-
-        [SerializeField]
-        [Header("SEミュート用ボタンを設定")]
-        Button _seMuteButton;
-
-        [SerializeField]
-        [Header("BGM調節用のスライダーを設定します")]
-        Slider _bgmSlider;
-
-        [SerializeField]
-        [Header("SE調節用のスライダーを設定します")]
-        Slider _seSlider;
-
-        [SerializeField]
         SoundView.SoundView _soundView;
 
         #region//フィールド
-        IObservableClickButton _observableClickButton;
-        IObservableSlider _observableSlider;
         ISoundModel _soundModel;
         #endregion
 
         [Inject]
         public void Construct(
-            IObservableClickButton observableClickButton,
-            IObservableSlider observableSlider,
             ISoundModel soundModel
         )
         {
-            _observableClickButton = observableClickButton;
-            _observableSlider = observableSlider;
             _soundModel = soundModel;
         }
 
         private void Start()
         {
+            _soundView.Initialize();
             Bind();
         }
 
         void Bind()
         {
             //---音声オプションを開く---
-            _observableClickButton.CreateObservableClickButton(_buttonToOpenCanvas)
+            _soundView.ButtonToOpenCanvas
                 .Subscribe(_ => _soundView.OpenSoundOption())
                 .AddTo(this);
-
             //---音声オプションを閉じる---
-            _observableClickButton.CreateObservableClickButton(_buttonToCloseCanvas)
+            _soundView.ButtonToCloseCanvas
                 .Subscribe(_ => _soundView.CloseSoundOption())
                 .AddTo(this);
 
-            _observableClickButton.CreateObservableClickButton(_overlayToCloseCanvas)
+            _soundView.OverlayToCloseCanvas
                 .Subscribe(_ => _soundView.CloseSoundOption())
                 .AddTo(this);
 
             //---ミュートボタン---
             //view to model
             //BGM
-            _observableClickButton.CreateObservableClickButton(_bgmMuteButton)
+            _soundView.BgmMuteButton
                 .Subscribe(_ => _soundModel.SetBgmIsMute(!_soundModel.BgmIsMute.Value))
                 .AddTo(this);
             //SE
-            _observableClickButton.CreateObservableClickButton(_seMuteButton)
+            _soundView.SEMuteButton
                 .Subscribe(_ => _soundModel.SetSEIsMute(!_soundModel.SEIsMute.Value))
                 .AddTo(this);
 
@@ -107,23 +71,23 @@ namespace SoundPresenter
             //---音声スライダー---
             //view to model
             //BGM
-            _observableSlider.CreateObservableSliderOnValueChanged(_bgmSlider)
+            _soundView.BgmSlider
                 .Subscribe(value => _soundModel.SetBgmVolume(value))
                 .AddTo(this);
 
             //SE
-            _observableSlider.CreateObservableSliderOnValueChanged(_seSlider)
+            _soundView.SESlider
                 .Subscribe(value => _soundModel.SetSEVolume(value))
                 .AddTo(this);
 
             //model to view
             //BGM
             _soundModel.BgmVolume
-                .Subscribe(value => _bgmSlider.value = value)
+                .Subscribe(value => _soundView.SetBgmSliderValue(value))
                 .AddTo(this);
             //SE
             _soundModel.SEVolume
-                .Subscribe(value => _seSlider.value = value)
+                .Subscribe(value => _soundView.SetSESliderValue(value))
                 .AddTo(this);
         }
     }
--- Assets/MyGames/Projects/Global/Sound/View/SoundView.cs
+++ Assets/MyGames/Projects/Global/Sound/View/SoundView.cs
@@ -1,29 +1,114 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Zenject;
 using UIUtility;
+using UniRx;
+
 
 namespace SoundView
 {
     public class SoundView : MonoBehaviour
     {
         [SerializeField]
+        [Header("音声オン時のアイコンを設定")]
+        Sprite _volumeOnSprite;
+
+        [SerializeField]
+        [Header("音声オフ時のアイコンを設定")]
+        Sprite _volumeOffSprite;
+
+        [SerializeField]
         [Header("サウンドオプションのキャンバスを設定します")]
         Canvas _soundOptionCanvas;
 
+        [SerializeField]
+        [Header("開くボタン(サウンドオプション)")]
+        Button _buttonToOpenCanvas;
+
+        [SerializeField]
+        [Header("閉じるボタン(サウンドオプション)")]
+        Button _buttonToCloseCanvas;
+
+        [SerializeField]
+        [Header("閉じるためのオーバーレイを設定します(サウンドオプション)")]
+        Button _overlayToCloseCanvas;
+
+        [SerializeField]
+        [Header("Bgmミュート用ボタンを設定")]
+        Button _bgmMuteButton;
+
+        [SerializeField]
+        [Header("SEミュート用ボタンを設定")]
+        Button _seMuteButton;
+
+        [SerializeField]
+        [Header("BGM調節用のスライダーを設定します")]
+        Slider _bgmSlider;
+
+        [SerializeField]
+        [Header("SE調節用のスライダーを設定します")]
+        Slider _seSlider;
+
         #region//フィールド
+        Image _bgmMuteIcon;
+        Image _seMuteIcon;
+        IObservableClickButton _observableClickButton;
+        IObservableSlider _observableSlider;
         IToggleableUI _toggleableUI;
         #endregion
 
+        //Observable化
+        //ボタン
+        public IObservable<Unit> ButtonToOpenCanvas;
+        public IObservable<Unit> ButtonToCloseCanvas;
+        public IObservable<Unit> OverlayToCloseCanvas;
+        public IObservable<Unit> BgmMuteButton;
+        public IObservable<Unit> SEMuteButton;
+        //スライダー
+        public IObservable<float> BgmSlider;
+        public IObservable<float> SESlider;
+
         [Inject]
         public void Construct(
+            IObservableClickButton observableClickButton,
+            IObservableSlider observableSlider,
             IToggleableUI toggleableUI
         )
         {
+            _observableClickButton = observableClickButton;
+            _observableSlider = observableSlider;
             _toggleableUI = toggleableUI;
         }
 
+        public void Initialize()
+        {
+            CreateObservable();
+            SetMuteIconImage();
+        }
+
+        /// <summary>
+        /// 各イベントをObservableにします
+        /// </summary>
+        void CreateObservable()
+        {
+            ButtonToOpenCanvas = _observableClickButton.CreateObservableClickButton(_buttonToOpenCanvas);
+            ButtonToCloseCanvas = _observableClickButton.CreateObservableClickButton(_buttonToCloseCanvas);
+            OverlayToCloseCanvas = _observableClickButton.CreateObservableClickButton(_overlayToCloseCanvas);
+            BgmMuteButton = _observableClickButton.CreateObservableClickButton(_bgmMuteButton);
+            SEMuteButton = _observableClickButton.CreateObservableClickButton(_seMuteButton);
+            BgmSlider = _observableSlider.CreateObservableSliderOnValueChanged(_bgmSlider);
+            SESlider = _observableSlider.CreateObservableSliderOnValueChanged(_seSlider);
+        }
+
+        void SetMuteIconImage()
+        {
+            _bgmMuteIcon = _bgmMuteButton.GetComponent<Image>();
+            _seMuteIcon = _seMuteButton.GetComponent<Image>();
+        }
+        
         public void OpenSoundOption()
         {
             _toggleableUI.OpenUIFor(_soundOptionCanvas?.gameObject);
@@ -34,18 +119,55 @@ namespace SoundView
             _toggleableUI.CloseUIFor(_soundOptionCanvas?.gameObject);
         }
 
+        public void SetBgmSliderValue(float value)
+        {
+            _bgmSlider.value = value;
+        }
+
+        public void SetSESliderValue(float value)
+        {
+            _seSlider.value = value;
+        }
+
         /// <summary>
         /// bgmのミュートアイコンを切り替えます
         /// </summary>
         /// <param name="isMute"></param>
         public void ToggleBgmMuteIcon(bool isMute)
         {
-            Debug.Log("togglebgm" + isMute);
+            ToggleMuteIcon(_bgmMuteIcon, isMute);
         }
 
+        /// <summary>
+        /// SEのミュートアイコンを切り替えます
+        /// </summary>
+        /// <param name="isMute"></param>
         public void ToggleSEMuteIcon(bool isMute)
         {
-            Debug.Log("togglese" + isMute);
+            ToggleMuteIcon(_seMuteIcon, isMute);
+        }
+
+        void ToggleMuteIcon(Image targetImage, bool isMute)
+        {
+            Sprite volumeIcon = GetVolumeIcon(isMute);
+            SetVolumeIcon(targetImage, volumeIcon);
+        }
+
+        /// <summary>
+        /// 音声アイコンを取得します
+        /// </summary>
+        /// <param name="isMute"></param>
+        /// <returns></returns>
+        Sprite GetVolumeIcon(bool isMute)
+        {
+            if (isMute) return _volumeOffSprite;
+            return _volumeOnSprite;
+        }
+
+        void SetVolumeIcon(Image targetImage, Sprite setSprite)
+        {
+            if (targetImage.sprite == setSprite) return;
+            targetImage.sprite = setSprite;
         }
     }
 }
