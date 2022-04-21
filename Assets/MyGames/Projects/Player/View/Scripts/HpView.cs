5923572fd85a376e8e2a4daba25fc5a0953fb57c
iwai32 <iwakou32@gmail.com>
2022-04-22 08:38:04 +0900

    UIにオーバーレイをかけて見やすくする

--- Assets/MyGames/Projects/Player/View/Scripts/HpView.cs
+++ Assets/MyGames/Projects/Player/View/Scripts/HpView.cs
@@ -10,6 +10,10 @@ namespace PlayerView
         [Header("HPのUIプレハブを設定")]
         private GameObject hpPrefab;//hpUIのプレハブ
 
+        [SerializeField]
+        [Header("HPUIの格納場所を設定")]
+        Transform hpTransform;
+
         /// <summary>
         /// HPの設定を行います
         /// </summary>
@@ -17,14 +21,14 @@ namespace PlayerView
         public void SetHpGauge(int hp)
         {
             //体力を一旦削除
-            for (int i = 0; i < transform.childCount; i++)
+            for (int i = 0; i < hpTransform.childCount; i++)
             {
-                Destroy(transform.GetChild(i).gameObject);
+                Destroy(hpTransform.GetChild(i).gameObject);
             }
             //現在の体力数分のゲージを作成
             for (int i = 0; i < hp; i++)
             {
-                Instantiate(hpPrefab, transform);//prefab, 配置場所
+                Instantiate(hpPrefab, hpTransform);//prefab, 配置場所
             }
         }
 
@@ -33,7 +37,7 @@ namespace PlayerView
         {
             for (int i = 0; i < damage; i++)
             {
-                Destroy(transform.GetChild(i).gameObject);
+                Destroy(hpTransform.GetChild(i).gameObject);
             }
         }
     }
